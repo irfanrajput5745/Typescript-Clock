7dfc08578c577998da73bde3deac76b88d783742
Muhammad Irfan <137328814+irfanrajput5745@users.noreply.github.com>
2024-02-06 06:43:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+"use strict";
+function clock() {
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let hrs = document.getElementById("hours");
+    let min = document.getElementById("minutes");
+    let scnd = document.getElementById("second");
+    let period = document.getElementById("ampm");
+    let day = document.getElementById("dd");
+    let date = document.getElementById("ddmmyy");
+    let time = new Date();
+    let h = time.getHours();
+    let m = time.getMinutes();
+    let s = time.getSeconds();
+    let d = time.getDay();
+    let dd = time.getDate();
+    let mm = time.getMonth();
+    let yy = time.getUTCFullYear();
+    if (h > 12) {
+        h = -12;
+    }
+    if (h == 0) {
+        h = 12;
+    }
+    let ampm = h <= 12 ? "AM" : "PM";
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    hrs.innerHTML = h;
+    min.innerHTML = m;
+    scnd.innerHTML = s;
+    period.innerHTML = ampm;
+    day.innerHTML = days[d];
+    date.innerHTML = months[mm] + " " + dd + " " + yy;
+}
+setInterval(clock, 1000);
+clock();
